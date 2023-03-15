2e666013d7cf6d67d97acd4d7075920e540a54ce
MrFelopes <feliperfragoso@gmail.com>
2023-03-15 16:40:53 -0300

    oiseaifhweifhwef

--- src/index.js
+++ src/index.js
@@ -5,50 +5,95 @@ import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import TakagiScreen from "./screens/TakagiScreen";
 import SplashScreen from "./screens/SplashScreen";
+import PoggersScreen from "./screens/PoggersScreen";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 
+const Stack = createNativeStackNavigator();
 
-const Stack = createNativeStackNavigator()
+export default function RootNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+            name="TabsNavigator"
+            component={TabsNavigation}
+            options={{
+                headerShown: false
+            }}
+        />
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{
+            title: "Tela Inicial",
+          }}
+        />
+        <Stack.Screen
+          name="AboutScreen"
+          component={AboutScreen}
+          options={{
+            headerTitle: "Tela de Sobre",
+          }}
+        />
+        <Stack.Screen
+          name="TakagiScreen"
+          component={TakagiScreen}
+          options={{
+            headerTitle: "Tela de Sobre",
+          }}
+        />
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{
+            headerTitle: "Tela de Login",
+          }}
+        />
+        <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
+          options={{
+            headerTitle: "Tela Splash",
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
-export default function RootNavigation(){
-    return(
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen 
-                    name="HomeScreen" 
-                    component={HomeScreen}
-                    options={{
-                        title: "Tela Inicial"
-                    }}
-                />
-                <Stack.Screen 
-                    name="AboutScreen" 
-                    component={AboutScreen}
-                    options = {{
-                        headerTitle: "Tela de Sobre"
-                    }}
-                />
-                <Stack.Screen 
-                    name="TakagiScreen" 
-                    component={TakagiScreen}
-                    options = {{
-                        headerTitle: "Tela de Sobre"
-                    }}
-                />
-                <Stack.Screen 
-                    name="LoginScreen" 
-                    component={LoginScreen}
-                    options = {{
-                        headerTitle: "Tela de Login"
-                    }}
-                />
-                <Stack.Screen 
-                    name="SplashScreen" 
-                    component={SplashScreen}
-                    options = {{
-                        headerTitle: "Tela Splash"
-                    }}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+const tabs = createMaterialBottomTabNavigator();
+
+function TabsNavigation() {
+  return (
+    <tabs.Navigator>
+      <tabs.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          title: "Tela Inicial",
+        }}
+      />
+      <tabs.Screen
+        name="LoginScreen"
+        component={LoginScreen}
+        options={{
+          title: "Tela de login",
+        }}
+      />
+      <tabs.Screen
+        name="TakagiScreen"
+        component={TakagiScreen}
+        options={{
+          title: "EU AMO TAKAGI",
+        }}
+      />
+      <tabs.Screen
+        name="PoggersScreen"
+        component={PoggersScreen}
+        options={{
+          title: "tela do poggers",
+        }}
+      />
+    </tabs.Navigator>
+  );
 }
--- src/screens/SplashScreen.jsx
+++ src/screens/SplashScreen.jsx
@@ -2,7 +2,7 @@ import { View } from "react-native";
 import {ActivityIndicator, Text} from "react-native-paper";
 import styles from "../utils/styles";
 
-export default function SplashScreen(){
+export default function SplashScreen({ navigation }){
     setTimeout(() => {
         navigation.navigate("HomeScreen"); 
     }, 3000);
@@ -11,9 +11,9 @@ export default function SplashScreen(){
   return(
     <View
       style={styles.container}
-    >
-      <Text>Carregando...</Text>
+      >
       <ActivityIndicator />
+      <Text>Carregando...</Text>
     </View>
   )
 }
