b7d897da6508b7f0a1305443d5edb8390146f1a8
MrFelopes <feliperfragoso@gmail.com>
2023-03-28 14:20:20 -0300

    b

--- src/screens/FetchNews.jsx
+++ src/screens/FetchNews.jsx
@@ -1,6 +1,7 @@
 import { View } from 'react-native';
 import {Text} from 'react-native-paper';
 import { useEffect } from 'react';
+import styles from '../utils/styles';
 
 export default function FetchNews(){
     function fetchNewsData() {
@@ -13,8 +14,14 @@ export default function FetchNews(){
         fetchNewsData();
     }, []);
     return(
-        <View>
+        <View style={styles.container}>
             <Text>Fetch News</Text>
+            {data.map((item) => (
+                <View key={item.id}>
+                    <Text key={item.id}></Text>
+                    <Text key={item.title}></Text>
+                </View> 
+            ))}
         </View>
     )
 }
